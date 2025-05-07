3b0bbe6bc42b4e71b77280a165fc5698ab5784dd
Azaduddin2001 <mdazaduddin2001@gmail.com>
2025-05-07 12:23:50 +0600

    Project10 comment

--- project10lab.java
+++ project10lab.java
@@ -1,3 +1,5 @@
 public class project10lab {
 public static void main(String[] args) {
    System.out.println("This is progect10");
+System.out.println("Hello World");
+System.out.println("This is progect10");
